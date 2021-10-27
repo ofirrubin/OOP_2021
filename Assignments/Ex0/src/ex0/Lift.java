1b3a104a09888914682800e7b0ce75360021d0bb
Ofir Rubin <ofirrubin02@gmail.com>
2021-10-27 11:03:18 +0300

    This version is actually worse (LiftV2) and for a reason, It does not have the stop calls picked. I now have to create the actual algorithm where it will save calls - pick and drop types, sort using extreme direction and then be able to pick them all, time should be better too.

--- Assignments/Ex0/src/ex0/Lift.java
+++ Assignments/Ex0/src/ex0/Lift.java
@@ -4,6 +4,93 @@ import ex0.algo.ElevatorAlgo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class LiftCallsV1{
+    Elevator e;
+    TreeSet<CallBundler> listCalls;
+    int time2Idle, minCall, maxCall;
+    int direction;
+    Comparator<CallBundler> upComp = (o1, o2) -> {
+        if (o1.getSrc() == o2.getSrc()) return 0;
+        else return o1.getSrc() > o2.getSrc() ? 1 : -1;
+};
+    Comparator<CallBundler> downComp = (o1, o2) -> {
+        if (o1.getSrc() == o2.getSrc()) return 0;
+        else return o1.getSrc() > o2.getSrc() ? -1 : 1;
+    };
+
+    public LiftCallsV1(Elevator e){
+        this.e = e;
+        direction = Elevator.LEVEL;
+        listCalls = new TreeSet<>(upComp);
+        time2Idle = 0;
+        minCall = e.getMinFloor();
+        maxCall = e.getMaxFloor();
+    }
+    public boolean hasCalls(){
+        return !listCalls.isEmpty();
+    }
+    public boolean inRange(int level){ // SRC calls range
+        return level <= maxCall && level >= minCall;
+    }
+    public int getPos(){
+        return e.getPos();
+    }
+    public double getSpeed(){
+        return e.getSpeed();
+    }
+    public int getState(){
+        return e.getState();
+    }
+    private int relativeDirection(int floor){
+        if (this.e.getPos() == floor) return Elevator.LEVEL;
+        else if (this.e.getPos() - floor > 0) return Elevator.UP;
+        else return Elevator.DOWN;
+    }
+
+    public void addCall(CallBundler b){
+        minCall = Math.min(minCall, b.getSrc());
+        maxCall = Math.max(maxCall, b.getSrc());
+        if (!hasCalls()) {
+            direction = relativeDirection(b.getSrc());
+        }
+        if (listCalls.size() == 1){
+            direction = relativeDirection(b.getSrc());
+            TreeSet<CallBundler> temp = new TreeSet<>(direction == Elevator.UP ? upComp: downComp);
+            temp.addAll(listCalls);
+            listCalls = temp;
+        }
+        time2Idle += b.time;
+        listCalls.add(b);
+
+    }
+    public void PopCall(){
+        if (! listCalls.isEmpty()){
+            e.goTo(listCalls.first().getSrc());
+            removeCall(listCalls.first());
+        }
+    }
+    public void removeCall(CallBundler b){
+        listCalls.remove(b);
+        time2Idle -= b.time;
+        if (listCalls.isEmpty()){
+            minCall = e.getMinFloor();
+            maxCall = e.getMaxFloor();
+        }
+        else{
+            minCall = Integer.MAX_VALUE;
+            maxCall = Integer.MIN_VALUE;
+            for (CallBundler c: listCalls) {
+                if (c.getDest() < minCall)
+                    minCall = c.getSrc();
+                if (c.getDest() > maxCall)
+                    maxCall = c.getSrc();
+            }
+        }
+    }
+}
 
 class CallBundler{
     public CallForElevator c;
@@ -14,13 +101,149 @@ class CallBundler{
         this.locateTo = locateTo;
         this.time = time;
     }
+    public int getSrc(){
+        return this.c.getSrc();
+    }
+    public int getDest(){
+        return this.c.getDest();
+    }
 }
 
-public class Lift implements ElevatorAlgo {
-    private Building building;
+class Lift implements ElevatorAlgo {
+    private final Building building;
+    LiftCallsV1[] calls;
+    public Lift(Building building){
+        this.building = building;
+
+    }
+    /**
+     * @return the Building on which the (online) elevator algorithm works on.
+     */
+    @Override
+    public Building getBuilding() {
+        return this.building;
+    }
+
+    /**
+     * @return The algorithm name
+     */
+    @Override
+    public String algoName() {
+        return "Lift algorithm";
+    }
+
+    /**
+     * This method is the main optimal allocation (aka load-balancing) algorithm for allocating the
+     * "best" elevator for a call (over all the elevators in the building).
+     *
+     * @param c the call for elevator (src, dest)
+     * @return the index of the elevator to which this call was allocated to.
+     */
+    @Override
+    public int allocateAnElevator(CallForElevator c) {
+        return 0;
+    }
+
+    /**
+     * This method is the low level command for each elevator in terms of the elevator API: GoTo, Stop,
+     * The simulator calls the method every time stamp (dt), note: in most cases NO action is needed.
+     *
+     * @param elev the current Elevator index on which the operation is performs.
+     */
+    @Override
+    public void cmdElevator(int elev) {
+    }
+}
+
+
+class LiftV2 implements ElevatorAlgo {
+    private final Building building;
+    LiftCallsV1[] calls;
+    public LiftV2(Building building){
+        this.building = building;
+        calls = new LiftCallsV1[building.numberOfElevetors()];
+        for (int i = 0; i < building.numberOfElevetors(); i++) {
+            calls[i] = new LiftCallsV1(building.getElevetor(i));
+        }
+    }
+    /**
+     * @return the Building on which the (online) elevator algorithm works on.
+     */
+    @Override
+    public Building getBuilding() {
+        return this.building;
+    }
+
+    /**
+     * @return The algorithm name
+     */
+    @Override
+    public String algoName() {
+        return "Lift algorithm";
+    }
+
+    /**
+     * This method is the main optimal allocation (aka load-balancing) algorithm for allocating the
+     * "best" elevator for a call (over all the elevators in the building).
+     *
+     * @param c the call for elevator (src, dest)
+     * @return the index of the elevator to which this call was allocated to.
+     */
+    @Override
+    public int allocateAnElevator(CallForElevator c) {
+        int maxElv = calls.length;
+        if (maxElv == 0) return 0;
+        int source = c.getSrc();
+        int dest = c.getDest();
+        double travelTime; // The time it takes for the elevator to arrive to the destination from its current point.
+        double elevatorTime = Integer.MAX_VALUE;
+        int temp;
+        int ste = 0; // Shortest time elevator
+        for(int i=0; i < maxElv; i ++){
+            LiftCallsV1 e = calls[i];
+            if (e.getState() == Elevator.ERROR) continue;
+            if (! e.hasCalls() || e.inRange(source)){
+                calls[ste].addCall(new CallBundler(c, ste, elevatorTime));
+                return ste;
+            }
+            travelTime = e.time2Idle + e.getSpeed() * Math.abs(source - dest);
+
+
+            if (e.getState() == Elevator.LEVEL)
+                temp = Math.abs(source - e.getPos());
+            else if (e.getState() == Elevator.DOWN)
+                temp = Math.abs(source - e.minCall);
+            else
+                temp = Math.abs(source - e.maxCall);
+            travelTime += Math.abs(e.listCalls.last().c.getDest()-source) * e.getSpeed();
+            if (travelTime + e.time2Idle > elevatorTime) continue;
+            ste = i;
+            elevatorTime = travelTime;
+        }
+        calls[ste].addCall(new CallBundler(c, ste, elevatorTime));
+        return ste;
+    }
+
+    /**
+     * This method is the low level command for each elevator in terms of the elevator API: GoTo, Stop,
+     * The simulator calls the method every time stamp (dt), note: in most cases NO action is needed.
+     *
+     * @param elev the current Elevator index on which the operation is performs.
+     */
+    @Override
+    public void cmdElevator(int elev) {
+        LiftCallsV1 c = calls[elev];
+        if (c.hasCalls()){
+            c.PopCall();
+        }
+    }
+}
+
+class LiftV1 implements ElevatorAlgo {
+    private final Building building;
     ArrayList<CallBundler> listCalls;
     int[] time2Idle;
-    public Lift(Building building){
+    public LiftV1(Building building){
         this.building = building;
         listCalls = new ArrayList<>();
         time2Idle = new int[this.building.numberOfElevetors()];
@@ -96,6 +319,7 @@ public class Lift implements ElevatorAlgo {
             i++;
             if (b.locateTo == elev){
                    e.goTo(b.c.getSrc());
+                   e.goTo(b.c.getDest());
                    listCalls.remove(b);
                    time2Idle[elev] -= b.time;
                    //e.stop(b.c.getDest());
