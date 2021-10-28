dc917ccb2f85a40396b79e5ce89e106b8db22b53
Ofir Rubin <ofirrubin02@gmail.com>
2021-10-28 23:30:35 +0300

    backup commit, trying another way.

--- Assignments/Ex0/src/ex0/algo/LiftAlgo.java
+++ Assignments/Ex0/src/ex0/algo/LiftAlgo.java
@@ -5,86 +5,121 @@ import ex0.CallForElevator;
 import ex0.Elevator;
 
 import java.util.Comparator;
+import java.util.Timer;
 import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
-class Call{
+class Call {
     boolean isSet; // picked up? dropped?
     int level;
-    public Call(int level, boolean isSet){
+
+    public Call(int level, boolean isSet) {
         this.level = level;
         this.isSet = isSet;
     }
 }
 
-class Lift{
+class Lift {
     Elevator e;
     boolean isUp;
     TreeSet<Call> iWay;
     TreeSet<Call> aWay;
     double staStoSeq;
     Comparator<Call> aWayComp = (o1, o2) -> {
-            if (o1.level == o2.level) return 0;
-            return o1.level > o2.level ? 1: -1; // If we are going down we sort from the highest to the lowest.
+        if (o1.level == o2.level) return 0;
+        return o1.level > o2.level ? 1 : -1; // If we are going down we sort from the highest to the lowest.
     };
     Comparator<Call> iWayComp = (o1, o2) -> {
         if (o1.level == o2.level) return 0;
-        return o1.level < o2.level ? 1: -1; // If we are going down we sort from the highest to the lowest.
+        return o1.level < o2.level ? 1 : -1; // If we are going down we sort from the highest to the lowest.
     };
-    public Lift(Elevator e){
+
+    public Lift(Elevator e, int startPos){
         this.e = e;
-        staStoSeq = e.getStopTime() + e.getTimeForOpen()+ e.getTimeForOpen() + e.getStartTime();
+        staStoSeq = e.getStopTime() + e.getTimeForOpen() + e.getTimeForOpen() + e.getStartTime();
         isUp = true;
-        e.goTo((int) Math.floor((e.getMaxFloor() + e.getMinFloor())/2.0)); // The middle floor is the best floor for extreme cases distance.
+        if (startPos > e.getMinFloor() && startPos < e.getMaxFloor())
+            e.goTo(startPos); // The middle floor is the best floor for extreme cases distance.
         iWay = new TreeSet<>(iWayComp);
         aWay = new TreeSet<>(aWayComp);
     }
-    public int getState(){
+
+    public Lift(Elevator e) {
+        this.e = e;
+        staStoSeq = e.getStopTime() + e.getTimeForOpen() + e.getTimeForOpen() + e.getStartTime();
+        isUp = true;
+        //e.goTo((int) Math.floor((e.getMaxFloor() + e.getMinFloor()) / 2.0)); // The middle floor is the best floor for extreme cases distance.
+        iWay = new TreeSet<>(iWayComp);
+        aWay = new TreeSet<>(aWayComp);
+    }
+
+    public int getState() {
         return e.getState();
     }
-    public int getPos(){
+
+    public int getPos() {
         return this.e.getPos();
     }
-    public int numOfCalls(){
+
+    public int numOfCalls() {
         return iWay.size() + aWay.size();
     }
-    public double estTime(int src, int dest){
+
+    public double estTime(int src, int dest) {
+        if (getPos() == src && relativeState(src, dest) == isUp) return 0;
         int stops = 1;
+        double est = 0;
         int l;
         if (iWay.isEmpty())
             l = src;
         else
             l = iWay.last().level;
-        if (getPos() < src && isUp){ // We are in the right direction and can pick them up.
-            for(Call c: iWay)
-                if(c.level < src) stops++;
+
+        // NOTE THAT BY COMPARING RELATIVE DIRECTION TO ISUP WE CHECK IF THE ELEVATOR IS GOING TO THE SAME WAY AS THE CALL, NOT NECESSARILY UP
+        // Calculating time to source from the current position (with the stops in the way), then calculating the time to the dest. with the stops on it's way.
+        if (getPos() < src && isUp) { // We are in the right direction and can pick them up.
+            for (Call c : iWay)
+                if (c.level < src) stops++;
                 else break;
-            return Math.abs(getPos() - src) * e.getSpeed() - stops * staStoSeq;
-        }
-        else if (getPos() > src && !isUp){
-            for(Call c: iWay)
-                if(c.level > src) stops++;
+            est += Math.abs(getPos() - src) / e.getSpeed() + stops * staStoSeq; // time to src
+        } else if (getPos() > src && !isUp) {
+            for (Call c : iWay)
+                if (c.level > src) stops++;
                 else break;
-            return Math.abs(getPos() - src) * e.getSpeed() - stops * staStoSeq;
-        }
-        else if(getPos() < src && !isUp){
+            est += Math.abs(getPos() - src) / e.getSpeed() + stops * staStoSeq;
+        } else if (getPos() < src && !isUp) {
             stops += iWay.size();
-            stops += aWay.size();
+            //stops += aWay.size();
+            for (Call c : aWay)
+                if (c.level < src) stops++;
+                else break;
             //return (Math.abs(getPos() - iWay.last().level) + Math.abs(level - iWay.last().level)) * e.getSpeed() - stops * staStoSeq;
-            return (Math.abs(getPos() - l) + Math.abs(src - l)) * e.getSpeed() - stops * staStoSeq;
-        }
-        else if(getPos() > src && isUp){
+            est += (Math.abs(getPos() - l) + Math.abs(src - l)) / e.getSpeed() + stops * staStoSeq;
+        } else if (getPos() > src && isUp) {
             stops += iWay.size();
-            stops += aWay.size();
+            //stops += aWay.size();
+            for (Call c : aWay)
+                if (c.level > src) stops++;
+                else break;
             //return (Math.abs(getPos() - iWay.last().level) + Math.abs(level - iWay.last().level)) * e.getSpeed() - stops * staStoSeq;
-            return (Math.abs(getPos() - l) + Math.abs(src - l)) * e.getSpeed() - stops * staStoSeq;
+            est += (Math.abs(getPos() - l) + Math.abs(src - l)) / e.getSpeed() + stops * staStoSeq;
 
         }
-        return 0;
+        if (l < dest) {
+            if (relativeState(l, dest) == isUp) //the last in that direction is at the same way as the last call. We have calculated the route so we just need to add the stop start time of this.
+            {
+                est += staStoSeq;
+            }
+        }
+        else if (l > dest) {
+
+        }
+        return est;
     }
 
 
-    public void addCall(int src, int dest){
-        if (iWay.size() == 0){
+    public void addCall(int src, int dest) {
+        if (iWay.size() == 0) {
             if (relativeState(src, dest)) {
                 swapComp();
                 isUp = relativeState(getPos(), src);
@@ -92,35 +127,37 @@ class Lift{
                 aWay = new TreeSet<>(aWayComp);
             }
         }
-        if (relativeState(getPos(), src) == isUp){ // Here the elevator source is it's current position and the destination is the pickup level.
+        if (relativeState(getPos(), src) == isUp) { // Here the elevator source is it's current position and the destination is the pickup level.
             iWay.add(new Call(src, false));
             if (relativeState(src, dest) == isUp)
                 iWay.add(new Call(dest, false));
             else
                 aWay.add(new Call(dest, false));
-        }
-        else{
+        } else {
             aWay.add(new Call(src, false));
             if (relativeState(src, dest) == isUp)
                 iWay.add(new Call(dest, false));
             else
                 aWay.add(new Call(dest, false));
         }
+        if (iWay.size() != 0 && aWay.size() != 0 && iWay.last() == aWay.first()){
+            aWay.remove(aWay.first());
+        }
     }
 
-    public boolean relativeState(int src, int dest){
+    public boolean relativeState(int src, int dest) {
         if (src == dest) return isUp; // It doesn't mather for the elevator, set it for the elevator "state"
         return dest > src;
     }
 
-    public void swapComp(){
+    public void swapComp() {
         Comparator<Call> temp = iWayComp;
         iWayComp = aWayComp;
         aWayComp = temp;
     }
 
-    public void cmdLift(){
-        if (iWay.size() == 1){
+    public void cmdLift() {
+        if (iWay.size() == 1) {
             e.goTo(iWay.first().level);
             swapComp();
             isUp = !isUp;
@@ -134,16 +171,17 @@ class Lift{
 
 }
 
-public class LiftAlgo implements ElevatorAlgo{
+public class LiftAlgo implements ElevatorAlgo {
     Building b;
     TreeSet<Lift> lifts;
-    public LiftAlgo(Building b){
+
+    public LiftAlgo(Building b) {
         this.b = b;
         lifts = new TreeSet<>(((o1, o2) -> {
             //if (o1.numOfCalls() == o2.numOfCalls()) return 0;
             if (o1.numOfCalls() == -1) return 0;
             //else
-            return o1.numOfCalls() <= o2.numOfCalls() ? 1: -1;
+            return o1.numOfCalls() <= o2.numOfCalls() ? 1 : -1;
         }));
         for (int i = 0; i < b.numberOfElevetors(); i++) {
             lifts.add(new Lift(b.getElevetor(i)));
@@ -178,14 +216,18 @@ public class LiftAlgo implements ElevatorAlgo{
     @Override
     public int allocateAnElevator(CallForElevator c) {
         Lift selectedLift = lifts.first();
-        double liftTime = Integer.MAX_VALUE, temp;
-        for(Lift l: lifts){
+        double liftTime = 20000, temp;
+        for (Lift l : lifts) {
+
             temp = l.estTime(c.getSrc(), c.getDest());
-            if (temp < liftTime){
+            //System.out.println("EST TIME WITH THIS ELEVATOR: " + temp + " WHILE SELECTED IS: " + liftTime + " THIS ELEVATOR IS BETTER? " + (temp < liftTime));
+            //if (temp > 70) continue;
+            if (temp < liftTime) {
                 selectedLift = l;
                 liftTime = temp;
             }
         }
+        System.out.println("SELECTED EST TIME: " + liftTime + " ELEVATOR: " + selectedLift.e.getID() + " / " + (b.numberOfElevetors()-1));
         selectedLift.addCall(c.getSrc(), c.getDest());
         return selectedLift.e.getID();
     }
@@ -198,8 +240,8 @@ public class LiftAlgo implements ElevatorAlgo{
      */
     @Override
     public void cmdElevator(int elev) {
-        for(Lift l: lifts){
-            if (l.e.getID() == elev){
+        for (Lift l : lifts) {
+            if (l.e.getID() == elev) {
                 l.cmdLift();
                 return;
             }
